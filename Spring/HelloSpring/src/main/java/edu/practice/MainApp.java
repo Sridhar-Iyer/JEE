fa3509198520bbe1b09786e57244ea5bafe8fca1
Sridhar <sreedharriyer@gmail.com>
2016-07-25 21:22:45 -0700

    Spring - Getting Started

---
+++ Spring/HelloSpring/src/main/java/edu/practice/MainApp.java
@@ -0,0 +1,16 @@
+package edu.practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+
+	}
+
+}
