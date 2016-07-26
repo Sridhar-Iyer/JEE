0b7de7e0fd1a4ba852d33fca04f94365f0b135cf
Sridhar <sreedharriyer@gmail.com>
2016-07-26 14:17:15 -0700

    added logging

--- Spring/StudentCollection/src/main/java/edu/practice/StudentMain.java
+++ Spring/StudentCollection/src/main/java/edu/practice/StudentMain.java
@@ -1,13 +1,17 @@
 package edu.practice;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class StudentMain {
+	
+	private static Logger log = Logger.getLogger(StudentMain.class.getName());
 
 	public static void main(String[] args) { 
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Student studentObject = (Student)context.getBean("student");
 		System.out.println(studentObject);
+		log.info(studentObject);
 	}
 }
